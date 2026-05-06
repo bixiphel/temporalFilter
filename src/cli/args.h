4f18de78bb46a998e68277e5a9901e4bff45e506
jacka10peil123@gmail.com <146150116+bixiphel@users.noreply.github.com>
2026-05-06 14:14:22 -0500

    Created header file for CLI interface.

---
+++ src/cli/args.h
@@ -0,0 +1,32 @@
+#ifndef ARGS_H
+#define ARGS_H
+
+typedef struct
+{
+	// File names
+	char input_file[256];	// String represention of the input file's name
+	char output_file[256];	// String rep. of the output file's name
+
+	// Filter name
+	char filter[256];	// Name of the filter used
+	
+	// Filter parameters
+	float sigma_s;		// Spatial spread for Spatiotemporal Gaussian Filter
+	float sigma_t;		// Temporal spread for Spatiotemporal Gaussian Filter
+
+} Config;
+
+// Function to initialize default values 
+void init_config(Config* cfg);
+
+// Parses arguments
+void parse_config(int argc, char** argv, Config* cfg);
+
+// Prints the current configuration (debug)
+void print_config(Config* cfg);
+
+// Print help message
+void print_help();
+
+
+#endif
